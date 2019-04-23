009bc6c72b85029a281e2ac9d43d457d7027f6e2
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2019-04-23 03:07:26 +0300

    hide BtnDownloadPresentation

--- components/shared/BtnDownloadPresentation.vue
+++ components/shared/BtnDownloadPresentation.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="mb20">
+  <!-- <div class="mb20">
     <v-btn outline large class="mx-0" slot="activator" :href="btnDownloadPresentation.url" target="_blank" :style="'width:' + customWidth">
-      <!-- <v-icon large left>{{ btnDownloadPresentation.icon }}</v-icon> -->
       <img :src="btnDownloadPresentation.img" alt="" width="25px" class="mr20">
       {{ btnDownloadPresentation.text }}
     </v-btn>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -16,7 +15,6 @@
     data () {
       return {
         btnDownloadPresentation: {
-          // icon: 'mdi-download',
           img: '/img/icons/download.svg',
           text: 'Скачать презентацию',
           url: 'https://firebasestorage.googleapis.com/v0/b/terapolis-db.appspot.com/o/hero%2FTerapolis--One-Page.pdf?alt=media&token=6fbcf708-214d-4d72-8120-5ba29e13e476'
